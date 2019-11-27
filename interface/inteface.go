8924956e36bea97e664b262c390e48b2d319fc8a
micolore <471304402@qq.com>
2019-11-27 20:36:40 +0800

    add interface

--- interface/inteface.go
+++ interface/inteface.go
@@ -5,6 +5,11 @@ import (
 	"fmt"
 )
 
+/**
+ *
+ * 1，接口用来 说明对象的行为
+ * 2，接口可以嵌套接口
+ */
 func main() {
 	fmt.Println("Hello interface!")
 
@@ -12,13 +17,17 @@ func main() {
 
 	phone = new(NokiaPhone)
 	phone.call()
+	phone.recive()
 
+	//动态的实现（多态）
 	phone = new(Ipone)
 	phone.call()
+	phone.recive()
 }
 
 type Phone interface {
 	call()
+	recive()
 }
 
 type NokiaPhone struct {
@@ -27,10 +36,19 @@ type NokiaPhone struct {
 func (nokiaPhone NokiaPhone) call() {
 	fmt.Println("i am  nokia i can call you")
 }
+func (nokiaPhone NokiaPhone) recive() {
+	fmt.Println("i am  nokiaPhone i can receive you")
+}
 
 type Ipone struct {
 }
 
+// 这行的作用是为了给iphone写call的实现
+// 其实就是给iphone加了一个call方法
 func (Ipone Ipone) call() {
 	fmt.Println("i am  iphone i can call you")
+}
+
+func (Ipone Ipone) recive() {
+	fmt.Println("i am  iphone i can receive you")
 }
